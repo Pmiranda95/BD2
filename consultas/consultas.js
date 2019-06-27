f6701228a6072e98df4b42559d719f99c072b97d
Pmiranda95 <pablo.miranda.alan95@gmail.com>
2019-06-27 16:16:41 -0300

    daf

--- consultas/consultas.js
+++ consultas/consultas.js
@@ -1,8 +1,29 @@
-db.Ventas.find({})
-   .projection({})
-   .sort({_id:-1})
-   .limit(100)
-   
+
+//CONSULTA 8 ESTA OK
+db.Ventas.aggregate([
+    {
+        $match:{
+            $and:[
+                {fecha:{$gte:ISODate("2018-03-31"),$lt:ISODate('2019-09-31')}},
+                {"sucursal.nombre":"sucursal_Lanus"}
+                ]
+            }
+    } ,
+    
+    { 
+        $project: { _id: "$cliente.dni",cliente:"$cliente",ventas:"$productoVendidos"} 
+    },
+    {
+         $unwind: "$ventas"
+     },
+    
+     { $group : { _id : "$cliente.dni","totalProductoVendido":{$sum:"$ventas.cantidad"} }}
+    {$sort:{'totalProductoVendido':-1}}
+     
+])
+
+
+
 //consulta 6 esta OK
    db.Ventas.aggregate([
     // crea un documento completo por cada element del array productoVendidos
@@ -20,8 +41,11 @@ db.Ventas.find({})
     
    ])
    
-//CONSULTA 8 ESTA OK
+
+//consulta 5
+
 db.Ventas.aggregate([
+    // crea un documento completo por cada element del array productoVendidos
     {
         $match:{
             $and:[
@@ -29,19 +53,14 @@ db.Ventas.aggregate([
                 {"sucursal.nombre":"sucursal_Lanus"}
                 ]
             }
-    } 
+    } ,
+    { $unwind: '$productoVendidos'},
+    { $group : { _id : "$productoVendidos.producto.descripcion"}},
+   { $project: {  totalMonto: { $multiply: [ "$productoVendidos.cantidad","$productoVendidos.producto.precio"] } } },
+    { $sort: {totalMonto: -1}}
     
-    { 
-        $project: { _id: "$cliente.dni",cliente:"$cliente",ventas:"$productoVendidos"} 
-    }
-    {
-         $unwind: "$ventas"
-     },
-    
-     { $group : { _id : "$cliente.dni","totalProductoVendido":{$sum:"$ventas.cantidad"} }}
-    {$sort:{'totalProductoVendido':-1}}
-     
-])
+   ])
+
 
 //consulta 4
 db.Ventas.aggregate([
@@ -52,7 +71,7 @@ db.Ventas.aggregate([
                 {"sucursal.nombre":"sucursal_Lanus"}
                 ]
             }
-    } 
+    },
     {
          $unwind: "$productoVendidos"
     },
@@ -60,6 +79,23 @@ db.Ventas.aggregate([
      { $group : { _id : "$productoVendidos.producto.tipo",total:{$sum:"$productoVendidos.producto.precio"}}}
     
     ])
+
+
+//consulta 3
+
+db.Ventas.aggregate([
+    {$match:
+        {
+        
+        $and: [
+            {"sucursal.nombre":{$in:["sucursal_avellaneda"]}},
+            { formaDePago:{$in:["tarjeta"]}},
+            {fecha:{$gte:ISODate("2018-03-31"),$lt:ISODate('2019-09-31')}}
+            ]
+        }
+    }, 
+    {$group: { _id: "$numeroTicker",total:{$sum:"$precioTotal"}}}
+])
     
 //consulta 2
 db.Ventas.aggregate([
@@ -70,7 +106,7 @@ db.Ventas.aggregate([
                 {"sucursal.nombre":"sucursal_Lanus"}
                 ]
             }
-    } 
+    },
     {
          $unwind: "$productoVendidos"
     },
@@ -78,3 +114,18 @@ db.Ventas.aggregate([
      { $group : { _id : "$cliente.obraSocial.nombre",total:{$sum:"$productoVendidos.producto.precio"}}}
     
     ])
+
+
+//CONSULTA 1
+db.Ventas.aggregate([
+    {$match:
+        {
+        
+        $and: [
+            {"sucursal.nombre":{$in:["sucursal_avellaneda"]}},
+            {fecha:{$gte:ISODate("2018-03-31"),$lt:ISODate('2019-09-31')}}
+            ]
+        }
+    }, 
+    {$group: { _id: "$numeroTicker",total:{$sum:"$precioTotal"}}}
+])
